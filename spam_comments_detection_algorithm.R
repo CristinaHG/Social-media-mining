cc8ce76e47d404f77aa692cf2cb1f519ad8cbbca
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-03-13 13:39:11 +0100

    getting comments from the post

--- spam_comments_detection_algorithm.R
+++ spam_comments_detection_algorithm.R
@@ -4,3 +4,12 @@ library(Rfacebook) # load library for mining FB
 page<-getPage("beach4all",token,n=500) # download latest 500 FB post
 post_id<-head(page$id,n=100) # get the 100 top post in page
 head(post_id,n=10)
+
+post_id<-as.matrix(post_id) # create matrix 
+allcomments<- ""
+for(i in 1:nrow(post_id)){
+  post<-getPost(post_id[i,],token,n=1000,likes = TRUE,comments = TRUE)
+  comments<-post$comments
+  allcomments<-rbind(allcomments,comments)
+}
+allcomments<-as.data.frame(allcomments)
