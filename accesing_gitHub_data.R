4ace0b4cda5259d631bdef5ba14c386139d04c11
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-03-31 19:58:32 +0200

    getting user repos and languages

--- accesing_gitHub_data.R
+++ accesing_gitHub_data.R
@@ -4,8 +4,13 @@ library(github)
 
 me<-get.myself(ctx)
 get.my.repositories(ctx=get.github.context())
-get.my.following(ctx)
-get.public.events(ctx=get.github.context())
-get.all.repositories(ctx=get.github.context())
+followers<-get.my.following(ctx)
+publicEvents<-get.public.events(ctx=get.github.context())
+someRepos<-get.all.repositories(ctx=get.github.context())
 get.user("approdriguez",ctx)
+
+aisharRepos<-get.user.repositories("approdriguez",ctx=get.github.context()) #get aishar repos
+# How much repos Aishar has?
+length(aisharRepos$content) #15
+usedLanguageInRepo<-get.repository.languages("approdriguez","Expert-System",ctx = get.github.context())
 
