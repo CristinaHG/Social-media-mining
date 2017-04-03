109c39c00758edb5438df25d488c264ff16498b9
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-04-03 20:54:41 +0200

    creating dataset with my friends

--- mostUsedLanguagesByFriends.R
+++ mostUsedLanguagesByFriends.R
@@ -8,12 +8,19 @@ myFollowers<-get.my.followers(ctx)
 numFollowing<-length(myFollowers$content)
 
 #creating a dataset with my followers
- dataset<-myFollowers$content[[1]]
+ dataset<-unlist(myFollowers$content[[1]])
  
  for(i in 2:length(myFollowers$content)){
-   dataset<-rbind(dataset,myFollowers$content[[i]])
+   dataset<-rbind(dataset,unlist(myFollowers$content[[i]]))
  }
+ 
+# myFollowers$content<-unname(myFollowers$content)
+# newDataSet<-sapply(1:length(myFollowers$content), function(i){
+#   (myFollowers$content[[i]])
+# })
+ 
  dataset<-unname(dataset)
  dataset<-as.data.frame(dataset)
  colnames(dataset)<-c(names(myFollowers$content[[1]]))
+ write.csv(dataset,file="CrisFollowers.csv")
  
