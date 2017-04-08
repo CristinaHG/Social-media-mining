284c558bb58d994cbb71decfd36c47cb8fd595f8
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-04-08 12:15:06 +0200

    implementing func to format date

--- mostUsedLanguagesByFriends.R
+++ mostUsedLanguagesByFriends.R
@@ -24,3 +24,30 @@ numFollowing<-length(myFollowers$content)
  colnames(dataset)<-c(names(myFollowers$content[[1]]))
  write.csv(dataset,file="CrisFollowers.csv")
  
+ # read the username column
+ myFriends<-read.csv("CrisFollowers.csv")
+ unname<-as.character(myFriends$login)     
+   
+ # extract data from friend's public repositories   
+ library(jsonlite)
+ library(stringr)
+ compdata<- ""
+ 
+ for(i in 1:nrow(myFriends)){
+   data2<-fromJSON(paste0("https://api.github.com/users/",str_trim(unname[i],side = "both"),"/repos?client_id&client_secret"))
+   if(length(data2)!=0){
+    data2<-data2[,-4]
+    compdata<-rbind(compdata,data2)
+   }
+   print(i)
+ }
+ 
+ write.csv(compdata,"ActiveUsers.csv")
+ 
+ activeusers<-read.csv("ActiveUsers.csv")
+ 
+ # make date format supported by R
+ date.formated<-function(datestring){
+   date<-as.POSIXct(datestring,format="%Y-%m-%dT%H:%M:%SZ",tz="GMT")
+ }
+ 
