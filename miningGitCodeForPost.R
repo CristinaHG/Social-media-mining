7bfced3dde8918013e9fef236221ba0853ec9b8e
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-04-21 13:31:00 +0200

    getting repos info dataset

--- miningGitCodeForPost.R
+++ miningGitCodeForPost.R
@@ -1,6 +1,6 @@
 require(devtools)
-install.packages('jsonlite')
-install_github("cscheid/rgithub")
+#install.packages('jsonlite')
+#install_github("cscheid/rgithub")
 
 # load libraries
 library(github)
@@ -19,7 +19,7 @@ myFollowers<-get.my.followers(context)
 # get number of my followers
 numFollowing<-length(myFollowers$content)
 
-#create a dataset with your followers
+# create a dataset with your followers
 dataset<-unlist(myFollowers$content[[1]])
 
 for(i in 2:length(myFollowers$content)){
@@ -31,4 +31,28 @@ dataset<-unname(dataset)
 dataset<-as.data.frame(dataset)
 colnames(dataset)<-c(names(myFollowers$content[[1]]))
 write.csv(dataset,file="CrisFollowers.csv")
+
+# read latest created csv 
+myFriends<-read.csv("CrisFollowers.csv")
+
+# extract the names
+unname<-as.character(myFriends$login)     
+
+# extract data from friend's public repositories   
+compdata<- NULL
+
+for(i in 1:nrow(myFriends)){
+  data<-fromJSON(paste0("https://api.github.com/users/",str_trim(unname[i],side = "both"),"/repos?clientID&clientSecret"))
+  if(length(data)!=0){
+    data<-data[,-4]
+    compdata<-rbind(compdata,data)
+  }
+  print(i)
+}
+
+# write data for reuse
+write.csv(compdata,"UsersWithRepoInfo.csv")
+
+# load data
+activeFriends<-read.csv("UsersWithRepoInfo.csv")
 
