7c2fc6e1d4a61b05266a0afb2a6f0306002def7e
CristinaHG <mrcrstnherediagmez@gmail.com>
2017-04-22 20:20:41 +0200

    doing data processing

--- miningGitCodeForPost.R
+++ miningGitCodeForPost.R
@@ -56,3 +56,25 @@ write.csv(compdata,"UsersWithRepoInfo.csv")
 # load data
 activeFriends<-read.csv("UsersWithRepoInfo.csv")
 
+# make date format supported by R
+date.format<-function(datestring){
+  date<-as.POSIXct(datestring,format="%Y-%m-%dT%H:%M:%SZ",tz="GMT")
+}
+
+# update dates with new format
+activeFriends$created_at<-date.format(activeFriends$created_at)
+activeFriends$updated_at<-date.format(activeFriends$updated_at)
+activeFriends$pushed_at<-date.format(activeFriends$pushed_at)
+
+# selecting just the interesting cols
+activeFriends<-activeFriends[,c("id","name","full_name","private","description","fork","created_at","updated_at","pushed_at","homepage","size","stargazers_count","watchers_count","language",
+                             "has_issues","has_downloads","forks_count","open_issues_count","forks","open_issues","watchers")]
+
+# replace T/F values by 0/1
+activeFriends$private<-as.integer(activeFriends$private)
+activeFriends$has_issues<-as.integer(activeFriends$has_issues)
+activeFriends$fork<-as.integer(activeFriends$fork)
+activeFriends$has_downloads<-as.integer(activeFriends$has_downloads)
+
+# Getting the username
+activeFriends$full_name<-unlist(lapply(strsplit(as.character(activeFriends$full_name),split = '/',fixed = TRUE),function(x) (x[1])))
